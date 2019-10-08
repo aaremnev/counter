93ca8d18771b7c4cb9bfdfa9f2dd8caed0b4a40f
aaremnev <stellargemm@gmail.com>
2019-10-08 23:10:48 +0600

    Fixed upload form

--- src/main/java/com/example/counter/rest/FileRest.java
+++ src/main/java/com/example/counter/rest/FileRest.java
@@ -4,7 +4,6 @@ import com.example.counter.services.CounterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
+@Slf4j
 @RestController
 @RequestMapping("/file")
-@Slf4j
 public class FileRest {
 
     private static final String TEXT_PLAIN = "text/plain";
@@ -27,24 +25,25 @@ public class FileRest {
     private CounterService counterService;
 
     @PostMapping
-    ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
 
         String name = file.getOriginalFilename();
         String text = new String(file.getBytes());
         String type = file.getContentType();
 
         if (file.isEmpty()) {
-            return new ResponseEntity<>("No file provided", HttpStatus.OK);
+            return new ResponseEntity<>("File is empty", OK);
         }
 
         if(!TEXT_PLAIN.equalsIgnoreCase(type)) {
-            return new ResponseEntity<>("Not allowed content type: " + type, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Not allowed content type: " + type, BAD_REQUEST);
         }
 
         counterService.process(name, text);
         long result = counterService.getCounter(name);
 
-        return new ResponseEntity<>("Successfully uploaded: " + result, new HttpHeaders(), OK);
+        String msg = String.format("Successfully uploaded: %s (%d)", name, result);
+        return new ResponseEntity<>(msg, new HttpHeaders(), OK);
     }
 
 }
